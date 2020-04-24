071dfae494f4e1f1801a1429f7c29a3b2e1d033f
T_wangyaxiong <2523682567@qq.com>
2020-04-24 21:26:38 +0800

    pytest学习

--- testing/test_pytest.py
+++ testing/test_pytest.py
@@ -1,5 +1,12 @@
+import yaml
+
 from python.calc import Calc
 import pytest
+
+
+def data():
+    with open("test_pyteset_data.yaml") as f:
+        return yaml.load(f)
 
 
 class TestCalc:
@@ -13,12 +20,13 @@ class TestCalc:
     4.0相加    
     '''
 
-    @pytest.mark.parametrize("a,b,result", [
-        (1, 2, 3),
-        (-1, -2, -3),
-        (0.1, 0.2, 0.3),
-        (0, 0, 0)
-    ])
+    # @pytest.mark.parametrize("a,b,result", [
+    #     (1, 2, 3),
+    #     (-1, -2, -3),
+    #     (0.1, 0.2, 0.3),
+    #     (0, 0, 0)
+    # ])
+    @pytest.mark.parametrize("a,b,result", data())
     def test_add(self, a, b, result):
         assert (self.calc.add(a, b), result)
 
@@ -37,3 +45,31 @@ class TestCalc:
     ])
     def test_div(self, a, b, result):
         assert (self.calc.div(a, b), result)
+
+#
+# def setup_module():
+#     pass
+#
+#
+# def teardown_module():
+#     pass
+
+
+# class TestCase:
+#     @classmethod
+#     def setup_class(cls):
+#         cls.calc = Calc()
+#
+#     @pytest.mark.demo1
+#     def test_case(self):
+#         assert (self.calc.add(1, 2), 3)
+#
+#     @pytest.mark.demo2
+#     def test_case1(self):
+#         assert (self.calc.div(1, 2), 0.5)
+#
+#     def setup(self):
+#         pass
+#
+#     def teardown(self):
+#         pass
